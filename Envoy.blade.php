190a3d6e5d029113ddd04dab7ed9718207260e6e
David García <desarrollador@clinicacolombiaes.com>
2017-07-21 11:46:01 -0500

    Deploy con zero downtime

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -3,4 +3,5 @@
 @task('deploy', ['on' => 'styde'])
 	cd /var/www/html/deploy_demo
 	git pull origin master
+	sudo /etc/init.d/httpd reload
 @endtask
--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 echo "Esta vez el deploy es con fabric :) desde Clínica Colombia <br>";
 
 echo "<p><strong>Este es un deploy con envoy, pero falta realizar el fast-rollback</srtong></p>";
+
+echo "Deploy con zero downtime, Gracias por tomar el curso";
