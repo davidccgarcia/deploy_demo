f2713721ffbab4dbfacd53ac25d44555b71666f4
David Garcia <ccristhiangarcia@gmail.com>
2017-07-23 20:26:21 -0500

    Corrección de errores en el deploy automático

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ from fabric.contrib.files import exists
 env.use_ssh_config = True
 env.hosts = ['styde']
 
-releases_dir = "var/www/html/deploy_demo/releases"
+releases_dir = "/var/www/html/deploy_demo/releases"
 git_branch = "master"
 git_repo = "https://github.com/davidccgarcia/deploy_demo.git"
 repo_dir = "/var/www/html/deploy_demo/repo"
@@ -19,48 +19,48 @@ current_release = "var/www/html/deploy_demo/current"
 
 @task
 def deploy():
-	init()
-	update_git()
-	create_release()
-	build_site()
-	swap_sysmlinks()
+    init()
+    update_git()
+    create_release()
+    build_site()
+    swap_sysmlinks()
 
 def init():
-	if not exists(releases_dir):
-		run("mkdir -p %s" % releases_dir)
+    if not exists(releases_dir):
+        run("mkdir -p %s" % releases_dir)
 
-	if not exists(repo_dir):
-		run("git clone -b %s %s %s" % (git_branch, git_repo, repo_dir))
+    if not exists(repo_dir):
+        run("git clone -b %s %s %s" % (git_branch, git_repo, repo_dir))
 
-	if not exists("%s/storage" % persist_dir):
-		run("mkdir -p %s/storage/app/public" % persist_dir)
-		run("mkdir -p %s/storage/framework/cache" % persist_dir)
-		run("mkdir -p %s/storage/framework/sessions" % persist_dir)
-		run("mkdir -p %s/storage/framework/views" % persist_dir)
-		run("mkdir -p %s/storage/logs" % persist_dir)
+    if not exists("%s/storage" % persist_dir):
+        run("mkdir -p %s/storage/app/public" % persist_dir)
+        run("mkdir -p %s/storage/framework/cache" % persist_dir)
+        run("mkdir -p %s/storage/framework/sessions" % persist_dir)
+        run("mkdir -p %s/storage/framework/views" % persist_dir)
+        run("mkdir -p %s/storage/logs" % persist_dir)
 
-def update_git()
-	with cd(repo_dir):
-		run("git checkout %s" % git_branch)
-		run("git pull origin %s" git_branch)
+def update_git():
+    with cd(repo_dir):
+        run("git checkout %s" % git_branch)
+        run("git pull origin %s" % git_branch)
 
 def create_release():
-	release_into = "%s/%s" % (releases_dir, next_release)
-	run("mkdir -p %s" %s release_into)
-	with cd(repo_dir):
-		run("git archive --worktree-attributes %s | tar -x -C %s" % (git_branch, release_into))
+    release_into = "%s/%s" % (releases_dir, next_release)
+    run("mkdir -p %s" % release_into)
+    with cd(repo_dir):
+        run("git archive --worktree-attributes %s | tar -x -C %s" % (git_branch, release_into))
 
 def build_site():
-	with cd("%s/%s" %s (releases_dir, next_release)):
-		run("Something")
+    with cd("%s/%s" % (releases_dir, next_release)):
+        run("Something")
 
 def swap_sysmlinks():
-	release_into = "%s/%s" % (releases_dir, next_release)
+    release_into = "%s/%s" % (releases_dir, next_release)
 
-	run("ln -nfs %s/.env %s/.env" % (persist_dir, release_into))
-	run("rm -rf %s/storage" % release_into)
-	run("ln -nfs %s/storage %s/storage" % (persist_dir, release_into))
+    run("ln -nfs %s/.env %s/.env" % (persist_dir, release_into))
+    run("rm -rf %s/storage" % release_into)
+    run("ln -nfs %s/storage %s/storage" % (persist_dir, release_into))
 
-	run("ln -nfs %s %s" % (release_into, current_release))
+    run("ln -nfs %s %s" % (release_into, current_release))
 
-	run("sudo service httpd reload")
+    run("sudo service httpd reload")
