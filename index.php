55b39040ce81830062bf86118b99cb8e23d4c4bf
David García <desarrollador@clinicacolombiaes.com>
2017-08-16 14:38:13 -0500

    Correcion de etiqueta strong en index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 echo "Esta vez el deploy es con fabric :) desde Clinica Colombia <br>";
 
-echo "<p><strong>Este es un deploy con envoy, pero falta realizar el fast-rollback</srtong></p>";
+echo "<p><strong>Este es un deploy con envoy, pero falta realizar el fast-rollback</strong></p>";
 
 echo "Deploy con zero downtime desde clinica colombia, Gracias por tomar el curso!";
 
