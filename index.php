6f51f600683c424aae7d1bcbc2cbf42e7b70470e
David García <desarrollador@clinicacolombiaes.com>
2017-08-16 14:29:01 -0500

    Cambio de acentos en index y nuevo mensaje para rollback

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-echo "Esta vez el deploy es con fabric :) desde Clínica Colombia <br>";
+echo "Esta vez el deploy es con fabric :) desde Clinica Colombia <br>";
 
 echo "<p><strong>Este es un deploy con envoy, pero falta realizar el fast-rollback</srtong></p>";
 
 echo "Deploy con zero downtime desde clinica colombia, Gracias por tomar el curso!";
 
 echo "Ahora estamos realizando el deploy";
+
+echo "<p>Rollback finalizado, es hora de poner en marcha en SIIS</p>";
